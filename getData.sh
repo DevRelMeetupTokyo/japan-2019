e547ebbee2d4ee7c1f36502182c3c18eaa4572e0
Atsushi <atsushi@moongift.jp>
2019-06-16 13:56:37 +0900

    Add form

---
+++ getData.sh
@@ -0,0 +1,2 @@
+curl -L "https://script.google.com/macros/s/AKfycbzayrErQKZ_tEom33d4FeY0hKHNEkji-BUs6BhS3hzBZXcdkTCU/exec?name=organizers" -o _data/organizers.json
+curl -L "https://script.google.com/macros/s/AKfycbzayrErQKZ_tEom33d4FeY0hKHNEkji-BUs6BhS3hzBZXcdkTCU/exec?name=sponsors" -o _data/sponsors.json
