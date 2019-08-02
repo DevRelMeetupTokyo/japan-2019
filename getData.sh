e2c41e3e60a64eb94ada669f34aca6f7a573212b
Atsushi <atsushi@moongift.jp>
2019-08-02 15:16:25 +0900

    Initial speakers

--- getData.sh
+++ getData.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 curl -L "https://script.google.com/macros/s/AKfycbzayrErQKZ_tEom33d4FeY0hKHNEkji-BUs6BhS3hzBZXcdkTCU/exec?name=organizers" -o _data/organizers.json
 curl -L "https://script.google.com/macros/s/AKfycbzayrErQKZ_tEom33d4FeY0hKHNEkji-BUs6BhS3hzBZXcdkTCU/exec?name=sponsors" -o _data/sponsors.json
+curl -L "https://script.google.com/macros/s/AKfycbzayrErQKZ_tEom33d4FeY0hKHNEkji-BUs6BhS3hzBZXcdkTCU/exec?name=sessions" -o _data/sessions.json
