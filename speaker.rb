5db995d9de73c1ce912b8610314446d60ce355ac
Atsushi <atsushi@moongift.jp>
2019-08-09 20:36:36 +0900

    panel

--- speaker.rb
+++ speaker.rb
@@ -1,5 +1,9 @@
 require('json')
 json = JSON.parse(open('./_data/sessions.json').read)
+JSON.parse(open('./_data/lts.json').read).each do |l|
+  json << l
+end
+
 json.each do |session|
   next unless session['speaker'] == 'true'
   content = <<-EOS
